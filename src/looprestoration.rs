75d852145a5afd2a0fec905ff5b8fcd5216e7e2c
Lilith River <lilith@imazen.io>
2026-03-29 19:40:44 -0600

    fix: convert scalar looprestoration fallbacks to compact buffer guards
    The 4 scalar fallback functions (wiener_rust, sgr_5x5_rust, sgr_3x3_rust,
    sgr_mix_rust) still used strided_slice_mut which creates stride-wide guards
    that overlap with concurrent tile threads. Convert to compact_read/write_back.

--- src/looprestoration.rs
+++ src/looprestoration.rs
@@ -587,10 +587,11 @@ fn wiener_rust<BD: BitDepth>(
     let round_bits_v = 11 - (bitdepth == 12) as c_int * 2;
     let rounding_off_v = 1 << round_bits_v - 1;
     let round_offset = 1 << bitdepth + (round_bits_v - 1);
-    let stride = p.pixel_stride::<BD>();
-    let (mut p_guard, p_base) = p.strided_slice_mut::<BD>(w, h);
+    let (mut compact, _) = p.compact_read::<BD>(w, h);
+    let pixels: &mut [BD::Pixel] =
+        FromBytes::mut_from_bytes(&mut compact[..]).expect("compact pixel reinterpretation");
     for j in 0..h {
-        let row_off = p_base.wrapping_add_signed(j as isize * stride);
+        let row_off = j * w;
         for i in 0..w {
             let mut sum = -round_offset;
             let z = &hor[j * REST_UNIT_STRIDE + i..(j + 7) * REST_UNIT_STRIDE];
@@ -599,10 +600,11 @@ fn wiener_rust<BD: BitDepth>(
                 sum += z[k * REST_UNIT_STRIDE] as c_int * filter[1][k] as c_int;
             }
 
-            p_guard[row_off + i] =
+            pixels[row_off + i] =
                 iclip(sum + rounding_off_v >> round_bits_v, 0, bd.into_c()).as_();
         }
     }
+    p.compact_write_back::<BD>(w, h, &compact);
 }
 
 /// Sum over a 3x3 area
@@ -977,16 +979,18 @@ fn sgr_5x5_rust<BD: BitDepth>(
     selfguided_filter(&mut dst, &mut tmp, w, h, 25, sgr.s0, bd);
 
     let w0 = sgr.w0 as c_int;
-    let stride = p.pixel_stride::<BD>();
-    let (mut p_guard, p_base) = p.strided_slice_mut::<BD>(w, h);
+    let (mut compact, _) = p.compact_read::<BD>(w, h);
+    let pixels: &mut [BD::Pixel] =
+        FromBytes::mut_from_bytes(&mut compact[..]).expect("compact pixel reinterpretation");
     for j in 0..h {
-        let row_off = p_base.wrapping_add_signed(j as isize * stride);
+        let row_off = j * w;
         for i in 0..w {
             let v = w0 * dst[j * 384 + i].as_::<c_int>();
-            p_guard[row_off + i] =
-                bd.iclip_pixel(p_guard[row_off + i].as_::<c_int>() + (v + (1 << 10) >> 11));
+            pixels[row_off + i] =
+                bd.iclip_pixel(pixels[row_off + i].as_::<c_int>() + (v + (1 << 10) >> 11));
         }
     }
+    p.compact_write_back::<BD>(w, h, &compact);
 }
 
 /// # Safety
@@ -1041,16 +1045,18 @@ fn sgr_3x3_rust<BD: BitDepth>(
     selfguided_filter(&mut dst, &mut tmp, w, h, 9, sgr.s1, bd);
 
     let w1 = sgr.w1 as c_int;
-    let stride = p.pixel_stride::<BD>();
-    let (mut p_guard, p_base) = p.strided_slice_mut::<BD>(w, h);
+    let (mut compact, _) = p.compact_read::<BD>(w, h);
+    let pixels: &mut [BD::Pixel] =
+        FromBytes::mut_from_bytes(&mut compact[..]).expect("compact pixel reinterpretation");
     for j in 0..h {
-        let row_off = p_base.wrapping_add_signed(j as isize * stride);
+        let row_off = j * w;
         for i in 0..w {
             let v = w1 * dst[j * 384 + i].as_::<c_int>();
-            p_guard[row_off + i] =
-                bd.iclip_pixel(p_guard[row_off + i].as_::<c_int>() + (v + (1 << 10) >> 11));
+            pixels[row_off + i] =
+                bd.iclip_pixel(pixels[row_off + i].as_::<c_int>() + (v + (1 << 10) >> 11));
         }
     }
+    p.compact_write_back::<BD>(w, h, &compact);
 }
 
 /// # Safety
@@ -1108,16 +1114,18 @@ fn sgr_mix_rust<BD: BitDepth>(
 
     let w0 = sgr.w0 as c_int;
     let w1 = sgr.w1 as c_int;
-    let stride = p.pixel_stride::<BD>();
-    let (mut p_guard, p_base) = p.strided_slice_mut::<BD>(w, h);
+    let (mut compact, _) = p.compact_read::<BD>(w, h);
+    let pixels: &mut [BD::Pixel] =
+        FromBytes::mut_from_bytes(&mut compact[..]).expect("compact pixel reinterpretation");
     for j in 0..h {
-        let row_off = p_base.wrapping_add_signed(j as isize * stride);
+        let row_off = j * w;
         for i in 0..w {
             let v = w0 * dst0[j * 384 + i].as_::<c_int>() + w1 * dst1[j * 384 + i].as_::<c_int>();
-            p_guard[row_off + i] =
-                bd.iclip_pixel(p_guard[row_off + i].as_::<c_int>() + (v + (1 << 10) >> 11));
+            pixels[row_off + i] =
+                bd.iclip_pixel(pixels[row_off + i].as_::<c_int>() + (v + (1 << 10) >> 11));
         }
     }
+    p.compact_write_back::<BD>(w, h, &compact);
 }
 
 #[deny(unsafe_op_in_unsafe_fn)]
